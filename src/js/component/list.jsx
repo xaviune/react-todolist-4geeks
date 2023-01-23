2073e3a4c99dbed6de059189307272694cdac052
xaviune <xaviune@gmail.com>
2023-01-23 21:58:29 +0000

    erase button hide fixed

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,31 +1,23 @@
-import React, {useState} from "react";
+import React from "react";
 import { XIcon } from "@primer/octicons-react";
 
 const List = (props) => {
 	const todos=props.todos;
 	const setTodos=props.setTodos;
-	const [Visibility, setVisivility] = useState({visibility: 'hidden'})
 	return (
 		<>
 		{todos.map ((item, index) => (
-			<div className="border border-secondary p-2"
-			onMouseEnter={
-				e => {setVisivility({visibility: 'visible'})}
-			}
-			onMouseLeave={
-				e => {setVisivility({visibility: 'hidden'})}
-			}
-			> {item}<div className="final"
+			<div className="border border-secondary p-2 show"> {item}<div className="final hide"
 			onClick={
 				() => 
 			setTodos(
 				todos.filter(
 					(t, currentIndex) =>
 						index != currentIndex
+			))}>
+				<XIcon size={24}/></div></div>
 			))}
-			>
-				<div style={Visibility}><XIcon size={24}/></div></div></div>
-			))}
+		
 		</>
 	);
 };
