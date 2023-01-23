c00b4194140f74dcb27bd8e364068fdafa182649
xaviune <xaviune@gmail.com>
2023-01-23 18:09:54 +0000

    actualización botón de borrar solo visible cuando el cursor este encima

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { XIcon } from "@primer/octicons-react";
 
 const List = (props) => {
 	const todos=props.todos;
 	const setTodos=props.setTodos;
+	const [Visibility, setVisivility] = useState({visibility: 'hidden'})
 	return (
 		<>
 		{todos.map ((item, index) => (
-			<div className="border border-secondary p-2"> {item}<div className="final"
+			<div className="border border-secondary p-2"
+			onMouseEnter={
+				e => {setVisivility({visibility: 'visible'})}
+			}
+			onMouseLeave={
+				e => {setVisivility({visibility: 'hidden'})}
+			}
+			> {item}<div className="final"
 			onClick={
 				() => 
 			setTodos(
 				todos.filter(
 					(t, currentIndex) =>
 						index != currentIndex
-			))}>
-				<XIcon size={24}/></div></div>
 			))}
-		
+			>
+				<div style={Visibility}><XIcon size={24}/></div></div></div>
+			))}
 		</>
 	);
 };
